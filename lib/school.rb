0d4696b718580961e19ee805caa323b640a31d05
jose luis <cortezluisjose@gmail.com>
2019-08-07 04:46:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,10 @@
 Class School
 attr_accessor :grade :student
+school["school_new"]=[]
+school["school_new"] << ""
 def initialize(school)
   @school=school
 end
 end
 school.roster
+school.add_student("Zach Morris", 9)
