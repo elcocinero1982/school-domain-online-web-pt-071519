6ae4cc263f4171436c0dd38eebe3241ff9220d6e
jose luis <cortezluisjose@gmail.com>
2019-08-07 04:47:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,6 +5,12 @@ school["school_new"] << ""
 def initialize(school)
   @school=school
 end
+def grade=(grade)
+  @grade=grade
+end
 end
 school.roster
-school.add_student("Zach Morris", 9)
+schoschool.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.rosterol.add_student("Zach Morris", 9)
